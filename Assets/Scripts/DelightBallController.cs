46187622a68c8babe7c51fa4538b2796ba0ecc3a
Marie Claire LeBlanc Flanagan <marieflanagan@gmail.com>
2022-07-19 19:38:30 -0400

    add avatar and collisions working

--- Assets/Scripts/DelightBallController.cs
+++ Assets/Scripts/DelightBallController.cs
@@ -6,19 +6,26 @@ public class DelightBallController : MonoBehaviour
 {
 	public Vector3 velocity = new Vector3(0, 0, 10);
 	public Vector3 rotateVelocity = new Vector3(100, 100, 0);
+	public float speedLimit = 50.0f;
+	private new Rigidbody rigidbody;
+	
 	
     // Start is called before the first frame update
     void Start()
     {
-        
+	    rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
 	void FixedUpdate()
-    {
+	{
+		rigidbody.velocity += this.velocity;
+		if(rigidbody.velocity.z >= speedLimit){
+			rigidbody.velocity = new Vector3(rigidbody.velocity.x, rigidbody.velocity.y, speedLimit);
+		}
 	    //transform.Translate(Vector3.forward * 10 * Time.deltaTime);
-	    transform.position += velocity * Time.deltaTime;
-	    transform.Rotate(rotateVelocity * Time.deltaTime);
+	    //transform.position += velocity * Time.deltaTime;
+	    //transform.Rotate(rotateVelocity * Time.deltaTime);
     }
     
 	void Update(){
--- Assets/Scripts/TouchBall.cs
+++ Assets/Scripts/TouchBall.cs
@@ -5,12 +5,13 @@ public class TouchBall : MonoBehaviour
     
 	void OnTriggerEnter(Collider other){
 		//Debug.Log(colllisionInfo.collider.name) 
-		Debug.Log(other.GetComponent<Collider>().name);
+		Debug.Log("whyyyyy");
 
 		if (other.gameObject.CompareTag("sphere"))
 		{
-			//other.gameObject.SetActive (false);
+			other.gameObject.SetActive (false);
 			Destroy(other.gameObject);
+			Debug.Log("destroyyyyyy");
 		}
 
 		//other.gameObject.transform.localScale = new Vector3(2f, 2f, 2f); 
