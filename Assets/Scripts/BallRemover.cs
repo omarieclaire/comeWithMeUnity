648ad65eebeb2abba54c6d2d7be914e80fc51b33
Marie Claire LeBlanc Flanagan <marieflanagan@gmail.com>
2022-07-21 18:29:46 -0400

    add scripts textures and scenes

---
+++ Assets/Scripts/BallRemover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallRemover : MonoBehaviour
+{
+	private void OnTriggerEnter(Collider other) {
+		
+		if (other.gameObject.CompareTag("sphere"))
+		{
+			
+			Destroy(other.gameObject);
+			
+		}
+	} 
+
+}
---
+++ Assets/Scripts/GarbageCollector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarbageCollector : MonoBehaviour
+{
+    
+    
+	// OnTriggerEnter is called when the Collider other enters the trigger.
+	protected void OnTriggerEnter(Collider other)
+	{
+		
+	}
+    
+    
+}
